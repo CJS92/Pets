7642ac50312ab641454f6daefd210c409ebd7786
Cody Sloan <46944830+CJS92@users.noreply.github.com>
2019-03-08 13:26:23 -0600

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,6 @@
+// This is the pet class
+
+static public int type;
+static public int weight;
+static public string owner;
+static public string name;
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Another project for IT 2040
+// Cody Sloan
+// 03/08/2019
+
+using System;
+
+namespace Pets
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
